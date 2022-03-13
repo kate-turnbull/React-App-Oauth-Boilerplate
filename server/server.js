f579153d8752594575647743abe25e263f6b6139
kate-turnbull <kate@kateturnbull.codes>
2022-03-13 14:29:23 +1300

    add in routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const path = require('path')
 const server = express()
 
+// require and use routes
+const routes = require('./routes')
+
+server.use('/', routes)
+
 // accept URL encoded data on POST requests
 server.use(express.urlencoded({ extended: false }))
 
@@ -11,11 +16,6 @@ server.use(express.static(staticFolder))
 
 server.get('/', (req, res) => {
     res.send('hello world!')
-})
-
-server.get('/path', (req, res) => {
-    const thisPath = path.join(__dirname, 'path.html')
-    res.sendFile(thisPath)
 })
 
 module.exports = server 
